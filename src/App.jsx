45bb7459d760fec494aeae80897dd30cdcd5660e
adityaarya03 <adityadrifter@icloud.com>
2025-06-18 12:20:14 +0530

    Initailising

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=' bg-black text-green-600 flex justify-center items-center h-screen w-screen'>
+      ConvoX
+    </div>
+  )
+}
+
+export default App
